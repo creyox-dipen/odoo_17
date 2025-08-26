22c1695cd0adaac23c22f46f5cf2759d986cffb4
dipen <dipen.creyox@gmail.com>
2025-08-26 18:53:33 +0530

    [NEW] cr_subscription_draft_invoice

---
+++ cr_subscription_draft_invoice/models/draft_invoice.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Creyox Technologies.
+
+from odoo import models, fields, api
+
+class SaleOrderTemplate(models.Model):
+    _inherit = "sale.order.template"
+    _description = "Add option to create draft invoice"
+
+    is_draft = fields.Boolean(string='Draft Invoice')
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _process_auto_invoice(self, invoice):
+        if self.sale_order_template_id.is_draft:
+            return
+        return super()._process_auto_invoice(invoice)
